7142e0ef77ea9b7d189c4a8ce81a0d30dc77d6eb
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-10-23 16:18:40 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ForgotPassword from './components/forgotPassword';
 import ResetPassword from './components/resetPassword';
+import Home from './components/home'
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+          <Route index element={<Home />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
         </Routes>
---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div>Password-Reset-Flow</div>
+  )
+}
+
+export default home
